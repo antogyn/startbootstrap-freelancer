73d9eceb43f5a1a315cb40bfd56261e22625d75c
aginiers <antogyn@gmail.com>
2015-04-12 03:59:26 +0200

    Rewrote the "Contact me" in Java

--- src/main/java/aginiers/freelancer/Contact.java
+++ src/main/java/aginiers/freelancer/Contact.java
@@ -1,6 +1,15 @@
 package aginiers.freelancer;
 
 import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,21 +29,52 @@ public class Contact extends HttpServlet {
      * Default constructor. 
      */
     public Contact() {
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+      String name = request.getParameter("name");
+      String phone = request.getParameter("phone");
+      String email = request.getParameter("email");
+      String message = request.getParameter("message");
+
+      Properties props = new Properties();
+      Session session = Session.getDefaultInstance(props, null);
+
+      try {
+          Message msg = new MimeMessage(session);
+          msg.setFrom(new InternetAddress("noreply@aginiers.com", "Contact"));
+          msg.addRecipient(Message.RecipientType.TO,
+                           new InternetAddress("antogyn@gmail.com", "Anthony Giniers"));
+          msg.setSubject("A new contact request has been sent : ");
+          String msgBody = ""
+              + "Name : " + name + "\n"
+              + "Phone : " + phone + "\n"
+              + "Email : " + email + "\n"
+              + "Message : \n" +  message;
+          msg.setText(msgBody);
+          Transport.send(msg);
+
+      } catch (AddressException e) {
+        e.printStackTrace();
+      } catch (MessagingException e) {
+        e.printStackTrace();
+      }
+      
+      
+      System.out.println(name + phone + email + message);
+      
+      response.sendRedirect("");
+      
 	}
 
 }
